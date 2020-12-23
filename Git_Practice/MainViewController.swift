588daca374cdd04a6e47a94f9233397f0c5ef3e0
SMizzz <shinn.mizzz@gmail.com>
2020-12-23 23:50:09 +0900

    test2 review - setup addviews setConstraints  added

--- Git_Practice/MainViewController.swift
+++ Git_Practice/MainViewController.swift
@@ -14,17 +14,34 @@ class MainViewController: UIViewController {
     lb.text = "테스트입니다."
     return lb
   }()
+  
   // MARK: - Init
   override func viewDidLoad() {
     super.viewDidLoad()
+    setup()
+    addViews()
+    setConstraints()
+  }
+  
+  // MARK: - Handlers
+  private func setup() {
     view.backgroundColor = .white
+  }
+  
+  private func addViews() {
     view.addSubview(label)
+  }
+  
+  private func setConstraints() {
+    labelConstraints()
+  }
+  
+  // MARK: - Constraints
+  private func labelConstraints() {
     label.translatesAutoresizingMaskIntoConstraints = false
     label.widthAnchor.constraint(equalToConstant: 200).isActive = true
     label.heightAnchor.constraint(equalToConstant: 100).isActive = true
     label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
     label.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
   }
-  // MARK: - Handlers
-  // MARK: - Constraints
 }
