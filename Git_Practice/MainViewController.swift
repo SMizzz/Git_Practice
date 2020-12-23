72df48b90702604d0c5bc8ba0f554250da4b6458
SMizzz <shinn.mizzz@gmail.com>
2020-12-23 23:45:27 +0900

    test2

--- Git_Practice/MainViewController.swift
+++ Git_Practice/MainViewController.swift
@@ -8,4 +8,23 @@
 import UIKit
 
 class MainViewController: UIViewController {
+  // MARK: - Properties
+  let label: UILabel = {
+    let lb = UILabel()
+    lb.text = "테스트입니다."
+    return lb
+  }()
+  // MARK: - Init
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .white
+    view.addSubview(label)
+    label.translatesAutoresizingMaskIntoConstraints = false
+    label.widthAnchor.constraint(equalToConstant: 200).isActive = true
+    label.heightAnchor.constraint(equalToConstant: 100).isActive = true
+    label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    label.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+  }
+  // MARK: - Handlers
+  // MARK: - Constraints
 }
